850ee108329e427d8589b485d6fdfa423b0e7cb9
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2026-08-06 17:35:23 +0000

    adiciona arquivos `database.js e `.env``

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const resultado = await database.query("SELECT 1 + 1 as soma;");
+  console.log(resultado.rows);
   response.status(200).json({ chave: "Os alunos do curso.dev são incríveis" });
 }
 
